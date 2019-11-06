bbe8a02c36ed13d0ba6fb5d30cb5eab2ec4cec52
SasiDharKM <sasidhar.km97@gmail.com>
2019-11-06 18:10:43 -0500

    added base for add severity scale

--- Medical/src/addSeverityScale.java
+++ Medical/src/addSeverityScale.java
@@ -10,4 +10,36 @@ public class addSeverityScale {
 		this.conn = con;
 	}
 	
+	public void displayMenu() {
+		System.out.println("1.There's another level to this scale");
+		System.out.println("2.There's no more levels, Go Back");
+		
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.println("Enter your choice to fill : ");		
+		int choice = input.nextInt();
+		
+		switch(choice) {
+		
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		default:
+		{
+			System.out.println("Please enter valid choice");
+		}
+		}
+		
+		input.close();
+	}
+	
 }
