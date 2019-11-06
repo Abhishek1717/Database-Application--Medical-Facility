0a5c3fdb1ab8dc56930a1f754694da3b82f40b3e
SasiDharKM <sasidhar.km97@gmail.com>
2019-11-05 19:50:13 -0500

    Finished boiler plate code of medical staff pages

--- Medical/src/addAssessmentRule.java
+++ Medical/src/addAssessmentRule.java
@@ -1,4 +1,83 @@
+import java.util.*;
+import java.sql.*;
 
 public class addAssessmentRule {
-
+	
+	Connection conn = null;
+	addAssessmentRule(Connection con){
+		System.out.println("Staff entering the assesment rule");
+		this.conn = con;
+	}
+	
+	public void listMenuOrder() throws SQLException {
+		
+		ArrayList<String> symptoms = new ArrayList<String>();
+		
+		patientCheckIn pci = new patientCheckIn(conn);
+		
+		Scanner input = new Scanner(System.in);
+		
+		Statement stmt = conn.createStatement();
+		
+		ResultSet rs = stmt.executeQuery("SELECT NAME from SYMPTOMS");
+		while(rs.next()) {
+			String sym = rs.getString("NAME");
+			symptoms.add(sym);
+		}
+		int i = 1;
+		for(i = 1; i<= symptoms.size(); i++) {
+			System.out.print(i);
+			System.out.println(". " + symptoms[i-1]);
+		}
+		System.out.println(i++);
+		System.out.println(". other");
+		
+		System.out.println(i);
+		System.out.println(". Done");
+		
+		System.out.println("Please enter your choice no: ");
+		
+		int choice = input.nextInt();
+		
+		if(choice == symptoms.size() + 1 ){
+			// I think not needed but review later.
+		}
+		else if(choice >= 1 && choice <= symptoms.size()) {
+			
+			//---> should display the appropriate severity scale for user to enter proper value
+		}
+		
+		
+		
+		System.out.println("1. High");
+		System.out.println("2. Normal");
+		System.out.println("3. Quarantine");
+		
+		System.out.println("Enter your choice to fill : ");		
+		choice = input.nextInt();
+		
+		switch(choice) {
+		
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		default:
+		{
+			System.out.println("Please enter valid choice");
+		}
+		}
+		
+		input.close();
+		
+		//---> go back to staff menu
+	}
 }
---
+++ Medical/src/treatedPatientList.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.sql.*;
+
+public class treatedPatientList {
+	
+	Connection conn;
+	
+	public treatedPatientList(Connection con) {
+		System.out.println("List of Treated patients\n");
+		this.conn = con;
+	}
+	
+	public void ListAllPatients() {
+		
+		// ---> sql statement to retrieve all treated patients and list them out
+		
+		ArrayList<String> patients = new ArrayList<String>();
+		
+		Scanner input = new Scanner(System.in);
+		
+		Statement stmt = conn.createStatement();
+		
+		ResultSet rs = stmt.executeQuery("REPLACE WITH APPROPRIATE CORRECT QUERY");
+		while(rs.next()) {
+			String sym = rs.getString("NAME"); // nothing is correct here
+			patients.add(sym);
+		}
+		int i = 1;
+		for(i = 1; i<= patients.size(); i++) {
+			System.out.print(i);
+			System.out.println(". " + patients[i-1]);
+		}
+		
+		System.out.println("Please enter your choice no: ");
+		
+		int choice = input.nextInt();
+		
+		if(choice >= 1 && choice <= patients.size()) {
+			
+			//---> should display the appropriate severity scale for user to enter proper value
+		}
+		
+		//after staff choose what patient display the below menu
+		
+		System.out.println("1. Checkout");
+		System.out.println("2. Go Back");
+		
+		System.out.println("Enter your choice to fill : ");		
+		choice = input.nextInt();
+		
+		switch(choice) {
+		
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		default:
+		{
+			System.out.println("Please enter valid choice");
+		}
+		}
+	}
+
+}
