4f4f5a111b60b8e2f90a29ab967b15d7d7122513
SasiDharKM <sasidhar.km97@gmail.com>
2019-11-07 18:45:25 -0500

    Completed base for negative experience

---
+++ Medical/src/negativeExperience.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.sql.*;
+
+public class negativeExperience {
+	
+	Connection conn = null;
+	public negativeExperience(Connection con) {
+		System.out.println("This is the negative experience page");
+		
+		this.conn =con;
+	}
+	
+	public void displayMenu() {
+		System.out.println("1. Negative Experience code");
+		System.out.println("2. Description");
+		System.out.println("3. Save");
+		System.out.println("4. Go Back");
+		
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.println("Enter your choice to fill : ");		
+		int choice = input.nextInt();
+		
+		switch(choice) {
+		
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		case 4:
+		{
+			break;
+		}
+		default:
+		{
+			System.out.println("Please enter valid choice");
+		}
+		}
+		
+		input.close();
+	}
+
+}
