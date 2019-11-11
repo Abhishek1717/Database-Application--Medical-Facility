210455000227b60cdbabe57eb63d6776ebf3ee97
SasiDharKM <sasidhar.km97@gmail.com>
2019-11-11 16:29:28 -0500

    modified referral reason

--- Medical/src/referralReason.java
+++ Medical/src/referralReason.java
@@ -4,6 +4,7 @@ import java.sql.*;
 public class referralReason {
 	
 	Connection conn = null;
+	String serviceName,Description,Reason;
 	
 	public referralReason(Connection con) {
 		System.out.println("This is the referral reason page");
@@ -15,13 +16,15 @@ public class referralReason {
 		//Display the reason code, 
 		//the name of the service 
 		//and description
-		
+		int i=0;
+		Scanner input = new Scanner(System.in); 
+		while(i<3)
+		{
 		System.out.println("1. Name of Service");
 		System.out.println("2. Description");
 		System.out.println("3. Record Reason");
 		System.out.println("4. Go Back");
 		
-		Scanner input = new Scanner(System.in); 
 		
 		System.out.println("Enter your choice to fill : ");		
 		int choice = input.nextInt();
@@ -29,27 +32,34 @@ public class referralReason {
 		switch(choice) {
 		
 		case 1:
-		{
+		{   System.out.println(" Enter Name of Service");
+		    serviceName=input.nextLine();
+		    i++;
 			break;
 		}
 		case 2:
-		{
+		{    System.out.println(" Enter description");
+			 Description=input.nextLine();
+			 i++;
 			break;
 		}
 		case 3:
-		{
+		{   
+			System.out.println(" Enter reason");
+			 Reason=input.nextLine();
+			 i++;
 			break;
 		}
 		case 4:
 		{
-			break;
+			return;
 		}
 		default:
 		{
 			System.out.println("Please enter valid choice");
 		}
 		}
-		
+	}
 		input.close();
 		
 	}
