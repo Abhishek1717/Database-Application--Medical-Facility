4601685bbfffc5fece610f7584a160e72948fd93
SasiDharKM <sasidhar.km97@gmail.com>
2019-11-11 16:29:50 -0500

    modified referral status

--- Medical/src/referralStatus.java
+++ Medical/src/referralStatus.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.sql.*;
 
 public class referralStatus {
-	
+	int facilityId,ReferrerId;
+	String reason;
 	Connection conn = null;
 	public referralStatus(Connection con) {
 		System.out.println("This is the referral status page");
@@ -12,12 +13,18 @@ public class referralStatus {
 	
 	public void displayMenu() {
 		
+		
+		int i=0;
+		Scanner input = new Scanner(System.in); 
+		while(i<3)
+		{
 		System.out.println("1. Facility ID");
 		System.out.println("2. Referrer ID");
 		System.out.println("3. Add reason");
 		System.out.println("4. Go Back");
 		
-		Scanner input = new Scanner(System.in); 
+		
+		
 		
 		System.out.println("Enter your choice to fill : ");		
 		int choice = input.nextInt();
@@ -26,19 +33,28 @@ public class referralStatus {
 		
 		case 1:
 		{
+			System.out.println("Enter Facility ID");
+			facilityId=input.nextInt();
+			i++;
 			break;
 		}
 		case 2:
-		{
+		{   
+			System.out.println("Enter Referrer ID");
+			ReferrerId=input.nextInt();
+			i++;
 			break;
 		}
 		case 3:
-		{
+		{   
+			referralReason rr= new referralReason(conn);
+			rr.displayMenu();
+			i++;
 			break;
 		}
 		case 4:
-		{
-			input.close();
+		{   
+			System.out.println("Redirecting back to patient Staff patient report");
 			return;
 		}
 		default:
@@ -46,6 +62,8 @@ public class referralStatus {
 			System.out.println("Please enter valid choice");
 		}
 		}
+		
+	}
 		
 		input.close();
 	}
